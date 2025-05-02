4b0b0aabbeed4717964df7638d9efe90c4b9621d
Magic-Forces <root@foxlab.pl>
2025-05-02 13:14:18 +0200

    Mobile init

---
+++ android/app/src/main/kotlin/pl/foxlab/photoseed/MainActivity.kt
@@ -0,0 +1,5 @@
+package pl.foxlab.photoseed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// Photoseed - gallery app
+// Copyright (C) 2025  magic-forces
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:flutter/material.dart';
+import 'package:photoseed/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Photoseed', home: Homepage());
+  }
+}
